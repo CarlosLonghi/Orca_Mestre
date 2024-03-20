80507a0bbd1b883c5e269bc4bb633af5ef96bf7f
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-03-20 16:25:42 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="bg-gray-950 w-full h-screen flex justify-center items-center">
+      <h1 className="text-4xl text-cyan-100">
+      Hello World!
+      </h1>
+    </div>
+  )
+}
