3c7ab2e791ed30177f12493a8b8d332387bd8847
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-03-21 17:23:23 -0300

    feat: add transactions table

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -6,6 +6,26 @@ export function Transactions() {
         <div>
             <Header />
             <Summary />
+
+            <main className="w-full max-w-app mt-16 mx-auto px-6">
+                <table className="w-full border-separate border-spacing-y-2">
+                    <tbody>
+                        <tr>
+                            <td className="flex-1">Venda de produto</td>
+                            <td className="text-green-300">R$ 1.200,00</td>
+                            <td>Venda</td>
+                            <td>21/03/2024</td>
+                        </tr>
+
+                        <tr>
+                            <td className="flex-1">Compra da semana</td>
+                            <td className="text-red-300">R$ -100,00</td>
+                            <td>Compra</td>
+                            <td>19/03/2024</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </main>
         </div>
     )
 }
