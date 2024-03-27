e0b2427b615b6f556ecaada4e1d7706a7611b039
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-03-27 14:57:36 -0300

    feat: get data from json server in transaction page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react";
+import { tv } from 'tailwind-variants'
+
 import { Header } from "../../components/header";
 import { Summary } from "../../components/summary";
 import { SearchForm } from "./SearchForm";
 
+interface Transaction {
+    id: number,
+    description: string,
+    type: 'income' | 'outcome',
+    category: string,
+    price: number,
+    createdAt: string
+}
+
+const transactionValueStyle = tv({
+    base: '',
+    variants: {
+        type: {
+            income: 'text-green-300',
+            outcome: 'text-red-300',
+        },
+    },
+});
+
 export function Transactions() {
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+
+    async function loadTransactions() {
+        const response = await fetch('http://localhost:3000/transactions')
+        const data = await response.json()
+
+        setTransactions(data)
+    }
+    useEffect(() => {
+        loadTransactions()
+    }, [])
+
     return (
         <div>
             <Header />
@@ -13,19 +47,16 @@ export function Transactions() {
 
                 <table className="w-full border-separate border-spacing-y-2">
                     <tbody>
-                        <tr>
-                            <td className="flex-1">Venda de produto</td>
-                            <td className="text-green-300">R$ 1.200,00</td>
-                            <td>Venda</td>
-                            <td>21/03/2024</td>
-                        </tr>
-
-                        <tr>
-                            <td className="flex-1">Compra da semana</td>
-                            <td className="text-red-300">R$ -100,00</td>
-                            <td>Compra</td>
-                            <td>19/03/2024</td>
-                        </tr>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td className="flex-1">{transaction.description}</td>
+                                    <td className={transactionValueStyle({ type: transaction.type })}>R$ {transaction.price}</td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
             </main>
