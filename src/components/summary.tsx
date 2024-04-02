967dc030873b6c6f8c4e068b5ca4c12923bd7d87
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-04-02 16:10:16 -0300

    feat: add calc transactions in component summary

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -4,8 +4,18 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from '@phosphor-icons/
 
 export function Summary() {
     const { transactions } = useContext(TransactionsContext)
-    console.log(transactions);
 
+    const summary = transactions.reduce((accumulator, transaction) => {
+        if (transaction.type === 'income') {
+            accumulator.income += transaction.price
+            accumulator.total += transaction.price
+        } else {
+            accumulator.outcome += transaction.price
+            accumulator.total -= transaction.price
+        }
+
+        return accumulator
+    }, {income: 0, outcome: 0, total: 0})
     return (
         <article className="w-full max-w-app m-auto -mt-20 px-6 grid grid-cols-3 gap-8">
             <div className='bg-gray-600 rounded-md py-6 px-8'>
@@ -14,7 +24,7 @@ export function Summary() {
                     <ArrowCircleDown className='w-8 h-8 text-green-300'/>
                 </header>
 
-                <strong className='block mt-4 text-2xl md:text-3xl'>R$ 1.000,00</strong>
+                <strong className='block mt-4 text-2xl md:text-3xl'>R$ {summary.income}</strong>
             </div>
 
             <div className='bg-gray-600 rounded-md py-6 px-8'>
@@ -23,7 +33,7 @@ export function Summary() {
                     <ArrowCircleUp className='w-8 h-8 text-red-300'/>
                 </header>
 
-                <strong className='block mt-4 text-2xl md:text-3xl'>R$ 1.000,00</strong>
+                <strong className='block mt-4 text-2xl md:text-3xl'>R$ {summary.outcome}</strong>
             </div>
 
             <div className='bg-green-700 rounded-md py-6 px-8'>
@@ -32,7 +42,7 @@ export function Summary() {
                     <CurrencyDollar className='w-8 h-8 text-gray-50'/>
                 </header>
 
-                <strong className='block mt-4 text-2xl md:text-3xl'>R$ 1.000,00</strong>
+                <strong className='block mt-4 text-2xl md:text-3xl'>R$ {summary.total}</strong>
             </div>
         </article>
     )
