3797b286518dff9f6e0f2a5f0fbb4995dbfe6c48
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-04-03 09:21:31 -0300

    refactor: create hook to calc transactions summary

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -1,22 +1,10 @@
-import { useContext } from 'react'
-import { TransactionsContext } from '../contexts/TransactionsContext'
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from '@phosphor-icons/react'
 import { priceFormatter } from '../utils/formatter'
+import { useSummary } from '../hooks/useSummary'
 
 export function Summary() {
-    const { transactions } = useContext(TransactionsContext)
-
-    const summary = transactions.reduce((accumulator, transaction) => {
-        if (transaction.type === 'income') {
-            accumulator.income += transaction.price
-            accumulator.total += transaction.price
-        } else {
-            accumulator.outcome += transaction.price
-            accumulator.total -= transaction.price
-        }
-
-        return accumulator
-    }, {income: 0, outcome: 0, total: 0})
+    const summary = useSummary()
+    
     return (
         <article className="w-full max-w-app m-auto -mt-20 px-6 grid grid-cols-3 gap-8">
             <div className='bg-gray-600 rounded-md py-6 px-8'>
