74064a5d25a8eb7c7cff70ec238d035c7b036dee
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-04-03 11:07:49 -0300

    feat: install and use hook React Hook Form in forms

--- src/components/newTransactionModal.tsx
+++ src/components/newTransactionModal.tsx
@@ -1,8 +1,35 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { ArrowCircleDown, ArrowCircleUp, X } from "@phosphor-icons/react";
 import * as Dialog from "@radix-ui/react-dialog";
 import * as RadioGroup from '@radix-ui/react-radio-group';
+import { Controller, useForm } from "react-hook-form";
+import * as z from 'zod'
+
+const newTransactionFormSchema = z.object({
+    description: z.string(),
+    price: z.number(),
+    category: z.string(),
+    type: z.enum(['income', 'outcome'])
+})
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
+    const {
+        control,
+        register,
+        handleSubmit,
+        formState: { isSubmitting }
+    } = useForm<NewTransactionFormInputs>({
+        resolver: zodResolver(newTransactionFormSchema)
+    })
+
+    async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        console.log(data);
+        
+    }
+
     return (
         <Dialog.Portal>
             <Dialog.Overlay className="fixed w-screen h-screen inset-0 bg-gray-900/75"/>
@@ -13,23 +40,59 @@ export function NewTransactionModal() {
                     <X className="w-6 h-6 absolute top-8 right-8 text-gray-300 transition hover:text-red-500 leading-none focus-visible:text-green-300"/>
                 </Dialog.Close>
 
-                <form action="" className="flex flex-col gap-4 mt-8">
-                    <input className="bg-gray-900 placeholder-gray-500 rounded-md border-none text-gray-300 p-4" type="text" placeholder="Descrição" required/>
-                    <input className="bg-gray-900 placeholder-gray-500 rounded-md border-none text-gray-300 p-4" type="number" placeholder="Preço" required/>
-                    <input className="bg-gray-900 placeholder-gray-500 rounded-md border-none text-gray-300 p-4" type="text" placeholder="Categoria" required/>
+                <form className="flex flex-col gap-4 mt-8" onSubmit={handleSubmit(handleCreateNewTransaction)}>
+                    <input 
+                        className="bg-gray-900 placeholder-gray-500 rounded-md border-none text-gray-300 p-4"
+                        type="text" 
+                        placeholder="Descrição" 
+                        required
+                        {...register('description')}
+                    />
+                    <input 
+                        className="bg-gray-900 placeholder-gray-500 rounded-md border-none text-gray-300 p-4"
+                        type="number" 
+                        placeholder="Preço" 
+                        required
+                        {...register('price', { valueAsNumber: true })}
+                    />
+                    <input 
+                        className="bg-gray-900 placeholder-gray-500 rounded-md border-none text-gray-300 p-4"
+                        type="text" 
+                        placeholder="Categoria" 
+                        required
+                        {...register('category')}
+                    />
 
-                    <RadioGroup.Root className="grid grid-cols-2 gap-4 mt-2">
-                        <RadioGroup.Item value="income" className="group bg-gray-700 text-gray-300 py-4 flex items-center justify-center gap-2 rounded-md transition ease-in-out data-[state=checked]:bg-green-700 data-[state=checked]:text-gray-50 data-[state=unchecked]:hover:bg-gray-600">
-                            <ArrowCircleDown className="w-6 h-6 text-green-500 group-data-[state=checked]:text-gray-50"/>
-                            Entrada
-                        </RadioGroup.Item>
-                        <RadioGroup.Item value="outcome" className="group bg-gray-700 text-gray-300 py-4 flex items-center justify-center gap-2 rounded-md transition ease-in-out data-[state=checked]:bg-red-700 data-[state=checked]:text-gray-50 data-[state=unchecked]:hover:bg-gray-600">
-                            <ArrowCircleUp className="w-6 h-6 text-red-500 group-data-[state=checked]:text-gray-50"/>
-                            Saida
-                        </RadioGroup.Item>
-                    </RadioGroup.Root>
+                    <Controller 
+                        control={control}
+                        name="type"
+                        render={({ field }) => {                            
+                            return (
+                                <RadioGroup.Root 
+                                    className="grid grid-cols-2 gap-4 mt-2"
+                                    onValueChange={field.onChange}
+                                    value={field.value}
+                                >
+                                    <RadioGroup.Item value="income" className="group bg-gray-700 text-gray-300 py-4 flex items-center justify-center gap-2 rounded-md transition ease-in-out data-[state=checked]:bg-green-700 data-[state=checked]:text-gray-50 data-[state=unchecked]:hover:bg-gray-600">
+                                        <ArrowCircleDown className="w-6 h-6 text-green-500 group-data-[state=checked]:text-gray-50"/>
+                                        Entrada
+                                    </RadioGroup.Item>
+                                    <RadioGroup.Item value="outcome" className="group bg-gray-700 text-gray-300 py-4 flex items-center justify-center gap-2 rounded-md transition ease-in-out data-[state=checked]:bg-red-700 data-[state=checked]:text-gray-50 data-[state=unchecked]:hover:bg-gray-600">
+                                        <ArrowCircleUp className="w-6 h-6 text-red-500 group-data-[state=checked]:text-gray-50"/>
+                                        Saida
+                                    </RadioGroup.Item>
+                                </RadioGroup.Root>
+                            )
+                        }}
+                    />
 
-                    <button className="w-full py-5 mt-2 text-gray-50 bg-green-500 font-bold rounded-md transition ease-in-out hover:bg-green-700" type="submit">Cadastrar</button>
+                    <button 
+                        className="w-full py-5 mt-2 text-gray-50 bg-green-500 font-bold rounded-md transition ease-in-out hover:bg-green-700 disabled:cursor-not-allowed disabled:opacity-60" 
+                        type="submit"
+                        disabled={isSubmitting}
+                    >
+                        Cadastrar
+                    </button>
                 </form>
             </Dialog.Content>
         </Dialog.Portal>
--- src/pages/Transactions/SearchForm.tsx
+++ src/pages/Transactions/SearchForm.tsx
@@ -1,12 +1,50 @@
-import { MagnifyingGlass } from '@phosphor-icons/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { CircleNotch, MagnifyingGlass } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+const searchFormSchema = z.object({
+    query: z.string()
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+    const { 
+        register,
+        handleSubmit,
+        formState: { isSubmitting }
+    } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFormSchema)
+    })
+
+    async function handleSearchTransactions(data: SearchFormInputs) {
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        console.log(data);
+        
+    }
+
     return (
-        <form className="flex gap-4">
-            <input className="flex-1 rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500" type="text" placeholder="Busque por transações..." />
+        <form 
+            className="flex gap-4" 
+            onSubmit={handleSubmit(handleSearchTransactions)}
+        >
+            <input 
+                className="flex-1 rounded-md border-none bg-gray-900 text-gray-300 p-4 placeholder:text-gray-500" 
+                type="text" 
+                placeholder="Busque por transações..." 
+                {...register('query')}
+            />
             <button 
-                className="flex items-center gap-3 p-4 border border-green-300 rounded-md text-green-300 font-bold transition hover:bg-green-500 hover:border-green-500 hover:text-gray-50">
-                <MagnifyingGlass weight='bold' className='w-5 h-5'/>
+                className="flex items-center gap-3 p-4 border border-green-300 rounded-md text-green-300 font-bold transition hover:bg-green-500 hover:border-green-500 hover:text-gray-50 disabled:cursor-not-allowed disabled:opacity-60"
+                disabled={isSubmitting}
+            >
+                {isSubmitting ?
+                    <CircleNotch weight='bold' className='animate-spin w-5 h-5'/>
+                    : 
+                    <MagnifyingGlass weight='bold' className='w-5 h-5'/>
+                }
+                
                 Buscar
             </button>
         </form>
